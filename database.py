1a2b637c8372425468d824cfeb16a644a4d21e59
Tony Le-Doan <doan951@gmail.com>
2026-08-29 13:26:39 -1000

    Added a sqlite database storage

---
+++ database.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+def sql_connect ():
+    connection = sqlite3.connect("internship.db")
+    cursor = connection.cursor()
+    return connection, cursor
+
+
+
+
+def sql_create_table(cursor):
+#creates a table only once that gives a unique ID to every internship in the database
+    cursor.execute("""CREATE TABLE IF NOT EXISTS internships (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name TEXT,
+        apply_by TEXT,
+        status TEXT
+    )
+    """)
+
+def sql_add_internship(cursor, connection, name, apply_by, status):
+    
+    cursor.execute(
+        """
+        INSERT INTO internships (name, apply_by, status)
+        VALUES (?, ? , ?)
+        """,
+        (name,apply_by,status)
+    )
+
+    connection.commit()
+
+def sql_get_internships(cursor):
+    cursor.execute("SELECT * FROM internships")
+    return cursor.fetchall()
+
+
+connection, cursor = sql_connect()
+sql_create_table(cursor)
+
+sql_add_internship(
+    cursor,
+    connection,
+    "Microsoft Explore",
+    "09/21/25",
+    "open"
+)
+
+internships = sql_get_internships(cursor)
+
+
+print(internships)
+
+connection.close()
