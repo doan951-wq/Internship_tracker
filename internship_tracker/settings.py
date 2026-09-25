59cfaf2d2248be80bcc87fc1410f37309182b438
Tony Le-Doan <doan951@gmail.com>
2026-09-24 14:49:52 -1000

    Configure Django to use PostgreSQL

--- internship_tracker/settings.py
+++ internship_tracker/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'internship_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'internship_tracker',
     }
 }
 
